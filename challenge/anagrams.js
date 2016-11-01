0a5b59428bb4c135fe38a8d4f30faa2e147874db
Tiffany Ip <tiffanyip@ucla.edu>
2016-10-31 22:42:04 -0700

    [complete] isAnagram

--- challenge/anagrams.js
+++ challenge/anagrams.js
@@ -9,6 +9,17 @@
 // isAnagram("why?","why not?") => false
 
 // Code:
-function isAnagram (a, b){
-
+function isAnagram (a, b) {
+  var a = a.replace(/\s+/g, '').split('');
+  var b = b.replace(/\s+/g, '').split('');
+  var i = 0;
+  while(i < b.length){
+    if(a.includes(b[i])) {
+      a.splice(a.indexOf(b[i]), 1);
+      i++;
+    } else {
+      return false;
+    }
+  }
+  return true;
 }
