90cf94f12a24a4f69503b743aaa31c602c6cc0fc
Tiffany Ip <tiffanyip@ucla.edu>
2016-11-01 00:44:52 -0700

    [complete] bubbleSort

--- challenge/bubbleSort.js
+++ challenge/bubbleSort.js
@@ -13,6 +13,12 @@
 
 // Code:
 
-var bubbleSort = function(array) {
-  
+var bubbleSort = function(arr) {
+  for (var i = 0; i < arr.length; i++){
+    for (var j = 0; j < arr.length-i-1; j++){
+      if (arr[j] > arr[j+1])
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+    }
+  }
+  return arr;
 };
